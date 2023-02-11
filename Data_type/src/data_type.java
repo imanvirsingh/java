3f3cab0b023bfb6b2c29087110343bdf2299a647
unknown <manvirfeb97@gmail.com>
2023-02-11 05:37:42 -0800

    tested first java variable STRING

--- Data_type/src/data_type.java
+++ Data_type/src/data_type.java
@@ -3,7 +3,9 @@ public class data_type {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		String name="manvir";
+		
+		System.out.println("my name is "+name+".");
+		}
 
 }
