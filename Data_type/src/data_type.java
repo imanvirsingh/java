23dc562497a88beb41e0d8ab5d3228cacb2f0752
unknown <manvirfeb97@gmail.com>
2023-02-11 16:00:26 -0800

    trying secon data type int

--- Data_type/src/data_type.java
+++ Data_type/src/data_type.java
@@ -3,9 +3,11 @@ public class data_type {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name="manvir";
+		String name="manvir"; // name is variable of String type
+		int age =26; //age is a variable of int type which holds 32 bit data
 		
-		System.out.println("my name is "+name+".");
+		System.out.println("my name is "+name+"and I'm "+age+" years old.");
+		
 		}
 
 }
