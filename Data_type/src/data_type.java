10f63ec18426439c9c19b37d79535744a496fadd
unknown <manvirfeb97@gmail.com>
2023-02-11 17:09:27 -0800

    tried all primitive data types

--- Data_type/src/data_type.java
+++ Data_type/src/data_type.java
@@ -4,9 +4,46 @@ public class data_type {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name="manvir"; // name is variable of String type
-		int age =26; //age is a variable of int type which holds 32 bit data
+		//Primitive data type
+		//Number types 
+		//integer type of number type primitive data type
+		byte myByte=125; // myBite is a variable of short data type which holds 8 bit data
+		short myShort= 5000; //myShort is a variable of short data type which holds 16 bit data
+		int myInt =26; //myInt is a variable of int type which holds 32 bit data and is most commonly used integer data type
+		long bigNumber=6556465653135555556L; //bigNumber is a variable of long data type, who's value ends with 'L'
 		
-		System.out.println("my name is "+name+"and I'm "+age+" years old.");
+		//Floating point type of number type primitive data type
+		float myFloat= 3.99f; //myFloat is a variable of float data type who's value ends with 'f'
+		double myDouble= 33.333333; //myDouble is a variable of double data type which can be ended with 'd'
+		
+		//Boolean Data Type
+		boolean myBoolean=true; //myBoolean is a variable of boolean type which either stores 'true' or 'false' 
+		
+		//Character Data Type
+		char myChar='a'; //myChar is a variable of char data type which stores a single character or an ASCII code enclosed in single quotes ''
+		String myName ="manvir"; //myName is variable of String data type which is non-primitive data type and it can store text is enclosed in double quotes
+		
+		System.out.println("trying primitive data types");
+		System.out.println("");
+		System.out.println("trying primitive integer numbers data type");
+		System.out.println("");
+		System.out.println("myByte = "+myByte+" is of byte data type");
+		System.out.println("myShort  = "+myShort+" is of Short data type");
+		System.out.println("myInt = "+myInt+" is of int data type");
+		System.out.println("bigNumber = "+bigNumber+" is of long data type");
+		System.out.println("");
+		System.out.println("trying primitive floating numbers data type");
+		System.out.println("myFloat = "+myFloat+" is of float data type");
+		System.out.println("myDoble = "+myDouble+" is of double data type");
+		System.out.println("");
+		System.out.println("trying Boolean Data type");
+		System.out.println("myBoolean = "+myBoolean+" is of boolean data type");
+		System.out.println("");
+		System.out.println("trying Character primitive data type");
+		System.out.println("myChar = "+myChar+" is of char data type");
+		System.out.println("");
+		System.out.println("trying non-primitive Character data type");
+		System.out.println("myName = "+myName+" is of String data type");
 		
 		}
 
