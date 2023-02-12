7788c8bf59c23c362f43fc5ce95ce853c33a7b54
unknown <manvirfeb97@gmail.com>
2023-02-11 22:46:55 -0800

    quick Exercise

--- JavaConrolFlow/src/SwitchCase.java
+++ JavaConrolFlow/src/SwitchCase.java
@@ -4,6 +4,25 @@ public class SwitchCase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// to see how Switch statement is executed
+		int Command=0;
+		
+		switch(Command) {
+		case 0:
+			System.out.println("CodeRunner running to North");
+			break;
+		case 1:
+			System.out.println("CodeRunner running to South");
+			break;
+		case 2:
+			System.out.println("CodeRunner running to East");
+			break;
+		case 3:
+			System.out.println("CodeRunner running to West");
+			break;
+			default :
+				System.out.println("CodeRunner running is confused doesn't know where to go");
+		}
+		
 		int balance=1000;
 		int amount=100;
 		int now;
