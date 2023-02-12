2d7b030031e4da3ffd8c467bcdbd0b89703c79db
unknown <manvirfeb97@gmail.com>
2023-02-11 19:46:57 -0800

    executed the if statement

--- JavaConrolFlow/src/JavaConrolFlow.java
+++ JavaConrolFlow/src/JavaConrolFlow.java
@@ -3,6 +3,12 @@ public class JavaConrolFlow {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int x=20;
+		int y=30;
+		// if statement which evaluates the given expression and runs the block of code if expression is true
+		if(x+y>45) {
+			System.out.println("x + y is greater than 45");
+		}
 
 	}
 
