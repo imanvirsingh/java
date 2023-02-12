cd4c387360a736060725cae97042310260c36625
unknown <manvirfeb97@gmail.com>
2023-02-11 20:27:47 -0800

    used all arithmetic and relational operations

--- JavaOperations/src/JavaOperations.java
+++ JavaOperations/src/JavaOperations.java
@@ -17,12 +17,75 @@ public class JavaOperations {
 		double div=a/b;
 		// Remainder
 		int rem=(int)(a%b);
-		
+		System.out.println("Using arithmetich operations");
 		System.out.println("addition a and b is "+sum);
 		System.out.println("Substraction of a and b is "+diff);
 		System.out.println("Multiplication of a and b is "+mult);
 		System.out.println("Quotient of a and b is "+div);
 		System.out.println("Remainder of a and b is "+rem);
+		
+		/*
+		 
+		 --relational operations--
+		   == --> Equal to
+		   != --> Not Equal to
+		   < --> Less than
+		   > --> greater than
+		   <= --> less than or equal to
+		   >= --> greater than to equal to 
+		   
+		 --logical operations-- 
+		  ! --> not
+		  && --> Logical AND
+		  || --> Logical OR
+		  
+		 */
+		
+		int x= 90;
+		int y= 10;
+		System.out.println("");
+		System.out.println("Using relational operations\n Using \"==\" relational operation ");
+		
+		if (x==y) {  // using "==" relation operation 
+			System.out.println("x is equal to y");
+			System.out.println("Using \"==\" operation \n in if block");
+			
+		}else {
+			System.out.println("x is not equal to y");
+			System.out.println("Using \"==\" operation \n in else block");
+		}
+		
+		if (x!=y) {  // using "==" relation operation 
+			System.out.println("x is  not equal to y");
+			System.out.println("Using \"!=\" operation \n in if block");
+		}else {
+			System.out.println("x is equal to y");
+			System.out.println("Using \"!=\" operation \\n in else block");
+		}
+		if(x>y) {
+			System.out.println("x is greater than y");
+			System.out.println("Using \">\" operation ");
+		}else {
+			System.out.println("y is greater than x \n Using \">\" operation ");
+		}
+		if(x<y) {
+			System.out.println("x is less than y");
+			System.out.println("Using \"<\" operation ");
+		}else {
+			System.out.println("y is less than x \n Using \">\" operation ");
+		}
+		if(x>=y) {
+			System.out.println("x is greater than or equal to y");
+			System.out.println("Using \">=\" operation ");
+		}else {
+			System.out.println("y is greater than or equal to  x \n Using \">=\" operation ");
+		}
+		if(x<=y) {
+			System.out.println("x is less than or equal to y");
+			System.out.println("Using \"<=\" operation ");
+		}else {
+			System.out.println("y is less than or equal to x \n Using \"<=\" operation ");
+		}
 		
 		
 	}
