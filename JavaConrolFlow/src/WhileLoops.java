8902b90ea389ed8b18dffb4208df5bf46033ba08
unknown <manvirfeb97@gmail.com>
2023-02-11 23:40:28 -0800

    executed the use of wile loop to print even numbers in first 10 numbers

--- JavaConrolFlow/src/WhileLoops.java
+++ JavaConrolFlow/src/WhileLoops.java
@@ -1,9 +1,14 @@
-
+	
 public class WhileLoops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int i=0;
+		System.out.println("Printing the list of even numbers in first 10 numbers");
+		while(i<=10) {
+			System.out.println(i);
+			i+=2;
+		}
 	}
 
 }
