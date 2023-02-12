82ffc7c76bfababd7feec66f0b7f2b7a0b92bf6a
unknown <manvirfeb97@gmail.com>
2023-02-11 19:16:46 -0800

    new project to test all java operations

---
+++ JavaOperations/src/JavaOperations.java
@@ -0,0 +1,30 @@
+
+public class JavaOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// JAVA Operators
+		//Arithmetic operators
+		double a= 20;
+		double b=50;
+		//addition 
+		int sum =(int)(a+b);
+		// Subtraction
+		int diff= (int)(a-b);
+		// Multiplication
+		int mult=(int)(a*b);
+		//Division
+		double div=a/b;
+		// Remainder
+		int rem=(int)(a%b);
+		
+		System.out.println("addition a and b is "+sum);
+		System.out.println("Substraction of a and b is "+diff);
+		System.out.println("Multiplication of a and b is "+mult);
+		System.out.println("Quotient of a and b is "+div);
+		System.out.println("Remainder of a and b is "+rem);
+		
+		
+	}
+
+}
